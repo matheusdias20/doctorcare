cf86de8cabba9e5f911a9731c1d4d78da81a9751
Matheus Vieira <mvdias20@gmail.com>
2022-05-04 22:10:35 -0300

    Section: Services, About, Contact and ScrollReveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,26 @@ function onScroll() {
         navigation.classList.remove('scroll')
     }
 }
+
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+    #home, 
+    #home img, 
+    #home .stats,
+    #services,
+    #services card,
+    #about,
+    #about img,
+`);
